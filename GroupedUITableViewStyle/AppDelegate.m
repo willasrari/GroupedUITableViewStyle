413e9d01aa9e1ced18e4d6409eae55e22fca9c02
Will Asrari <will.asrari@gmail.com>
2013-02-02 11:10:22 -0800

    removed references to old viewControllers

--- GroupedUITableViewStyle/AppDelegate.m
+++ GroupedUITableViewStyle/AppDelegate.m
@@ -1,5 +1,4 @@
 #import "AppDelegate.h"
-//#import "ViewController.h"
 #import "HossViewController.h"
 
 @implementation AppDelegate
@@ -13,15 +12,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
-
-    /*
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController_iPhone" bundle:nil] autorelease];
-    } else {
-        self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController_iPad" bundle:nil] autorelease];
-    }
-    */
-    
     self.viewController = [[[HossViewController alloc] initWithNibName:@"HossViewController" bundle:nil] autorelease];
     
     self.window.rootViewController = self.viewController;
